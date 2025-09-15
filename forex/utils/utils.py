f36d4a708bd625b379af6e9add10bccb7ee96d0b
Ethan Pedersen <45214966+ethanp55@users.noreply.github.com>
2025-09-15 14:14:27 -0600

    SM for ICMLA 2025

--- forex/utils/utils.py
+++
@@ -1,3 +0,0 @@
-YEARS = list(range(2013, 2023))
-CURRENCY_PAIRS = ['Eur_Usd']
-TIME_FRAMES = ['H1']
--- stag_hare/agents/prey.py
+++
@@ -1,8 +0,0 @@
-from stag_hare.agents.random_agent import Random
-from utils.utils import HARE_NAME, STAG_NAME
-
-
-class Prey(Random):
-    def __init__(self, name: str) -> None:
-        assert name == HARE_NAME or name == STAG_NAME
-        Random.__init__(self, name)
